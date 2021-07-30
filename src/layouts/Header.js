d2c9bf5d5f36b6d537c8b88970158f05106b03a3
renc <cangaprogram@gmail.com>
2021-07-30 15:48:09 +0300

    added conditional linking

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,9 +2,20 @@ import React from "react";
 import logo from "../storage/canon-logo.png";
 import {Link} from "react-router-dom";
 
-//TODO: Make conditional Linking
 
 function Header(){
+
+    const ConditionalLink  = localStorage.getItem('token');
+    console.log(ConditionalLink)
+
+    const renderButton = () => {
+        if (ConditionalLink) {
+            return <Link to="/auth/user" className="nav-link text-white p-3 m-2 fw-bold" ><i className="bi bi-person-circle"></i></Link>
+        } else {
+            return <Link to="/auth/login" className="nav-link text-white p-3 m-2 fw-bold" >LOGIN</Link>
+        }
+    }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container p-4">
@@ -17,7 +28,7 @@ function Header(){
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav flex-row justify-content-evenly">
                         <Link to="/shop" className="nav-link text-white p-3 m-2 fw-bold">STORE</Link>
-                        <Link to="/auth/login" className="nav-link text-white p-3 m-2 fw-bold" >LOGIN</Link>
+                        {renderButton()}
                         <Link to="/shop/cart" className="nav-link text-white p-3 m-2 " ><i className="bi bi-bag"/></Link>
                     </div>
                 </div>
